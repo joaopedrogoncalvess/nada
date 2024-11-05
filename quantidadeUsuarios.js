067b5fd3baf406b416f8d8b62df033d809a4bf27
joaopedrogoncalvess <131706900+joaopedrogoncalvess@users.noreply.github.com>
2024-11-05 10:54:18 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,3 +1,4 @@
+import { getCSS, tickConfig} from "./common.js"
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -5,15 +6,50 @@ async function quantidadeUsuarios() {
     quantidadeUsuarios()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
+    const layout = {
+
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+            color: getCSS('--primary-color'),
+            family: getCSS('--font'),
+            size: 30
+        },
+        xaxis: {
+            tickfont: {
+                color: getCSS('--primary-color'),
+                size: 16,
+                family: getCSS('--font'),
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                color: getCSS('--secundary-color')
+                 }
+                }
+            },
+            yaxis: {
+                title: {
+                    text: 'bilhões de usuários ativos',
+                        font: {
+                        color: getCSS('--secundary-color')
+                        }
+                    }
+            }
+
+}
+            }
+        
+        }
     const data = [
         {
             x: nomeDasRedes,
             y: quantidadeUsuarios,
-            type: 'bar'
+            type: 'bar',    
             marker: {
-                color: getComputedStyle(document.body).
-                
-                getPropertyValue('--primary-color')
+                color:getCSS('--primary-color')
                 
                 }
         }
@@ -21,7 +57,7 @@ async function quantidadeUsuarios() {
         const grafico = document.createElement('div')
         grafico.className = 'grafico'
         document.getElementById('graficos-container').appendChild(grafico)
-        Plotly.newPlot(grafico, data)
+        Plotly.newPlot(grafico, data ,layout)
 
     console.log(dados)
     }
