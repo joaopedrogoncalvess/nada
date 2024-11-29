cad46b4ee2a0f270219b5a2a3ee5f5718d2866d0
joaopedrogoncalvess <131706900+joaopedrogoncalvess@users.noreply.github.com>
2024-11-29 11:36:51 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -7,4 +7,16 @@ const getCSS = (variavel) => {return getComputedStyle(document.body).getProperty
         family: getCSS('--font')
         }
     
-export {getCSS, tickConfig}
+export {getCSS, tickConfig,criarGrafico,incluirTexto} 
+
+function incluirTexto (texto) {
+    const container = document.getElementById
+    ('graficos-container');
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('   graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+
+}
+criarGrafico(data, layout)
+incluirTexto(`Embora o <span>Instagram</span> ocupea quarta posição de redes sociais com mais usuáriosno mundo, ela é a rede social de que as pessoas maisgostam, seguida pelo WhatsApp e pelo Facebook. `)
--- redesFavoritasMundo.js
+++ redesFavoritasMundo.js
@@ -1,3 +1,4 @@
+import {getCSS, criarGrafico} from "./common.js"    
 async function redesFavoritasMundo() {
     const url = 'https://raw. githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
     const res = await fetch(url)
@@ -8,3 +9,68 @@ async function redesFavoritasMundo() {
     }
     
     redesFavoritasMundo()
+    const data = [
+        {
+        
+        values: valores,
+        labels: redes,
+        type: 'pie',
+        textinfo: 'label+percent'
+        
+        }
+        ]
+        import {getCSS} from "./common.js"
+        const layout = {
+            plot_bgcolor: getCSS('--bg-color'),
+            paper_bgcolor: getCSS('--bg-color'),
+            height: 700,
+            title: {
+                text: 'Redes sociais com mais usuários no mundo',
+                x: 0,
+                font: {
+                    color: getCSS('--primary-color'),
+                    family: getCSS('--font'),
+                    size: 30
+                }
+            },
+            legend: {
+                font: {
+                color: getCSS('--primary-color'),
+                size: 16
+                }
+                },
+            xaxis: {  
+                tickfont: tickConfig
+                ,
+                
+                title: {
+                    text: 'nome das redes sociais',
+                    font: {
+                    color: getCSS('--secundary-color')
+                    }
+                }
+            },
+                yaxis: {
+                    tickfont: tickConfig,
+                    title: {
+                    text: 'bilhões de usuários ativos',
+                    font: {
+                    color: getCSS('--secundary-color')
+                }
+                }
+                }
+                
+        }
+       function criarGrafico(data, layout){
+        const grafico = document.createElement('div')
+        grafico.className = 'grafico'
+        document.getElementById('graficos-container')
+        .appendChild(grafico)
+        const config = {
+            responsive: true,
+            displayModeBar: false
+            }
+        Plotly.newPlot(grafico, data, layout, config)
+    }
+    
+    export {getCSS, tickConfig, criarGrafico}
